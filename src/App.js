a08c5a09065c81de8e3f2b4e68fa50ba6b7e14c2
maystar236 <70463399+maystar236@users.noreply.github.com>
2020-12-10 21:51:38 +1100

    Add Weather CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <p>Weather App</p>
+      <Weather />
       <a href="https://github.com/maystar236/React-Weather-Application" target="_blank">Open-sourced code</a>
       </div>
     </div>
