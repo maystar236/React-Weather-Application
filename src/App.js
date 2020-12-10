3417279194bdc8174b84f0b1cad93c875b1818a1
maystar236 <70463399+maystar236@users.noreply.github.com>
2020-12-10 21:34:33 +1100

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <p>Weather App</p>
       <a href="https://github.com/maystar236/React-Weather-Application" target="_blank">Open-sourced code</a>
-      
+      </div>
     </div>
   );
 }
