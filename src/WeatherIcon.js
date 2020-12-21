c94fc0d826b93ccd9afe8080b6b0a13a5b7bc7cd
maystar236 <70463399+maystar236@users.noreply.github.com>
2020-12-21 14:17:43 +1100

    Icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function WeatherIcon(props) {
     return (
-        <img src="src/Images/${response.data.weather[0].icon}.png" alt={props.alt}/>
+        <img src="src/Images/${response.data.weather[0].icon}.png"/>
     );
 }
