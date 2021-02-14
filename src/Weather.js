91d01ab39a552bfa613e5ea34b850ff4c87c38f0
maystar236 <70463399+maystar236@users.noreply.github.com>
2021-02-14 14:32:58 +1100

    Geolocation work

--- src/Weather.js
+++ src/Weather.js
@@ -22,6 +22,7 @@ export default function Weather(props) {
       sunrise: response.data.sys.sunrise,
       sunset: response.data.sys.sunset,
       icon: response.data.weather[0].icon,
+      timezone: response.data.timezone
     });
   }
   
@@ -35,26 +36,24 @@ function handleCityChange(event) {
 }
 
 function search() {
-  const apiKey = "f0fc9549c6de17fa6c965f916fc7d8d4";
+  let apiKey = "f0fc9549c6de17fa6c965f916fc7d8d4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
+}
   
-  function getCurrentLocation(event) {
+  function getLocation(event) {
     event.preventDefault();
-    navigator.geolocation.getCurrentPosition(current);
+    navigator.geolocation.getCurrentPosition(showLocation);
   }
 
-  function current(position) {
-    let lon = position.coords.longitude;
-    let lat = position.coords.latitude;
-    let unit = "metric";
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&units=${unit}`;
-    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+  function showLocation(position) {
+    let apiKey = "f0fc9549c6de17fa6c965f916fc7d8d4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   
   }
 
-  }
+  
 
 if (weatherData.ready) {
   return (
@@ -82,13 +81,10 @@ if (weatherData.ready) {
             className="current-location-button"
             id="current-location-button"
             value="Current location"
-            onClick={getCurrentLocation}
+            onClick={getLocation}
          />
          
-        <button type="button" id="location-search">          
-        <input type="submit" className="search-button" value="Location" />
-        </button>
-      
+     
       <WeatherInfo data={weatherData}/>
 
 
